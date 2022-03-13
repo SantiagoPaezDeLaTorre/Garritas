8d9e6289c298a89e2ed47e6b0d73b3fdbc5baec7
SantiPaez1 <santipaez96@outlook.com>
2022-03-13 14:32:39 -0300

    slider

--- GarritasPetshop/myapp/src/components/Carousel/Carousel.js
+++ GarritasPetshop/myapp/src/components/Carousel/Carousel.js
@@ -1,49 +1,50 @@
-import {Carousel} from 'react-bootstrap';
+import { Carousel } from 'react-bootstrap';
 import slide1 from '../../assets/images/slide1.jpg';
 import slide2 from '../../assets/images/slide2.jpg';
 import slide3 from '../../assets/images/slide3.jpg';
 import slide4 from '../../assets/images/slide4.jpg';
 import slide5 from '../../assets/images/slide5.jpg';
+import "./Carousel.css";
 
-const Slider = () =>{
-    return(
-        <Carousel>
-    <Carousel.Item>
-        <img
-            className="d-block w-100"
-            src={slide1}
-            alt="First slide"
-        />
-    </Carousel.Item>
-    <Carousel.Item>
-        <img
-            className="d-block w-100"
-            src={slide2}
-            alt="Second slide"
-        />
-    </Carousel.Item>
-    <Carousel.Item>
-        <img
-            className="d-block w-100"
-            src={slide3}
-            alt="Third slide"
-        />
-    </Carousel.Item>
-    <Carousel.Item>
-        <img
-            className="d-block w-100"
-            src={slide4}
-            alt="Fourth slide"
-        />
-    </Carousel.Item>
-    <Carousel.Item>
-        <img
-            className="d-block w-100"
-            src={slide5}
-            alt="Fifth slide"
-        />
-    </Carousel.Item>
-</Carousel>
+const Slider = () => {
+    return (
+        <Carousel variant='dark'>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={slide1}
+                    alt="First slide"
+                />
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={slide2}
+                    alt="Second slide"
+                />
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={slide3}
+                    alt="Third slide"
+                />
+            </Carousel.Item>
+            <Carousel.Item> 
+                <img
+                    className="d-block w-100"
+                    src={slide4}
+                    alt="Fourth slide"
+                />
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={slide5}
+                    alt="Fifth slide"
+                />
+            </Carousel.Item>
+        </Carousel>
     )
 }
 
